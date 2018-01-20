093b7bb096440965d4a13da40963af2ea31fce5b
Mahesh Ranaweera <mi6softlab@gmail.com>
2018-01-20 12:14:27 -0500

    email

--- _app/email.php
+++ _app/email.php
@@ -1,25 +1,36 @@
 <?php
     /**
-     * Mail to admin about the registration
+     * Mail to admin about the registration and stud
      */
     function sendMail($strEmail, &$content, $msgType){
         $to=$strEmail;
+        $studEmail = $content['email'];
         $subject = "";
 
         $message = "<html><body>";
+        $stud_msg= "<html><body>";
 
         switch($msgType){
             case "student_signup":
-                $message .= "<h1>Student Registration/h1>";
+                /**Admin email */
+                $message .= "<h1>Student Registration</h1>";
                 $message .= "<p>Name: ".$content['name']."</p>";
                 $message .= "<p>Email: ".$content['email']."</p>";
                 $message .= "<p>School: ".$content['school']."</p>";
                 $message .= "<p>Field: ".$content['field']."</p>";
                 $message .= "<p>Contact: ".$content['contact']."</p>";       
                 $message .= "<code>Student Password: ".$content['stud_passwd']."</code>";
+
+                /**Student email */
+                $stud_msg .= "<h1>Student Registration</h1>";
+                $stud_msg .= "<p>Thank you ".$content['name']." for joining A/L e-Learning</p>";
+                $stud_msg .= "<p>We will be contacting you shortly for further information</p>";
+                #$stud_msg .= "<code>Your Account Password: ".$content['stud_passwd']."</code>";
+
                 $subject = "Student Registration"; 
                 break;
             case "career_signup":
+                /**Admin email */
                 $message .= "<h1>Career Application</h1>";
                 $message .= "<p>Name: ".$content['name']."</p>";
                 $message .= "<p>Email: ".$content['email']."</p>";
@@ -27,6 +38,13 @@
                 $message .= "<p>Professional Qualification: ".$content['prof_qual']."</p>";
                 $message .= "<p>Contact: ".$content['contact']."</p>";   
                 $subject = "Career Application";
+
+                /**Career email */
+                $stud_msg .= "<h1>Career Application</h1>";
+                $stud_msg .= "<p>Thank you ".$content['name']." for applying A/L e-Learning</p>";
+                $stud_msg .= "<p>Your application was successfully submitted</p>";
+                $stud_msg .= "<p>We will be contacting you shortly for further information</p>";
+
                 break;
             default:
                 $message .= "";
@@ -34,6 +52,11 @@
         }
         
         $message .= "</body></html>";
+        $stud_msg .= "</body></html>";
+
+        /**Comment after testing */
+        echo $message;
+        echo $stud_msg;
 
         $headers = "Content-Type: text/html; charset=ISO-8859-1\r\n";
         $headers .= "From: StudentPortal <studentportal.com>\r\n";
@@ -41,13 +64,17 @@
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "X-Mailer: PHP/" . phpversion();
         
-        if(mail($to, $subject, $message, $headers)){
+        /**Send emails */
+        if(mail($to, $subject, $message, $headers) && mail($studEmail, $subject, $stud_msg, $headers)){
             return 1;
         }else{
             return 0;
         }
     }
 
+    /**
+     * Student password recovery
+     */
     function recoveryEmail($strEmail, $recKey){
         $to=$strEmail;
         $subject = "Account Recovery"; 
